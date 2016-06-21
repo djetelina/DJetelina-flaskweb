8a6c1ebe7e762c3afff32106e46b1f7e5279f51b
David Jetelina <djt@seznam.cz>
2016-06-21 14:01:16 +0200

    oops old imports

--- run.py
+++ run.py
@@ -1,9 +1,5 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-from flask.ext.login import LoginManager
-from flask.ext.openid import OpenID
-from config import basedir
-import os
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
