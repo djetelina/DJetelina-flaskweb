39b9d0f1e83c23fea926175187d2ffdcd6e66087
David Jetelina <djt@seznam.cz>
2016-06-21 19:46:59 +0200

    psql wtf

--- Procfile
+++ Procfile
@@ -1,4 +1,4 @@
-web: python run.py
+web: python app.py
 init: python migrate.py db init
 migrate: python migrate.py db migrate
 upgrade: python app.py db upgrade
--- config.py
+++ config.py
@@ -3,6 +3,5 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
 CSRF_ENABLED = True
 SECRET_KEY = 'icecream'
