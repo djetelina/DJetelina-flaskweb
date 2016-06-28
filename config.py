0b87467d1de42d442c599a9b19e77716f37a7037
David Jetelina <djt@seznam.cz>
2016-06-28 02:30:59 +0200

    oh and security

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
 CSRF_ENABLED = True
-SECRET_KEY = 'icecream'
+SECRET_KEY = os.environ.get('FLASK_KEY')
