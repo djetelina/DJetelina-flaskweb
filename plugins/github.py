ff46d2ddf88ec2c50c5472c727c8183365e222c3
David Jetelina <david@djetelina.cz>
2016-07-13 01:56:08 +0200

    github api cache fix

--- plugins/github.py
+++ plugins/github.py
@@ -9,8 +9,8 @@ def fetch_commits(repo_name):
     :return:
     """
     requests_cache.install_cache('commits_cache', expires_after=60 * 15)
-    r = requests.get('https://api.github.com/repos/iScrE4m/{}/commits'.format(repo_name)).json()
-    return r
+    r = requests.get('https://api.github.com/repos/iScrE4m/{}/commits'.format(repo_name))
+    return r.json()
 
 
 class Commits:
