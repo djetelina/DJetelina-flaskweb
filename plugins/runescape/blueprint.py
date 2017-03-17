e15834181fc2e132ffe8946a8140442c0d01034c
David Jetelina <david.jetelina@firma.seznam.cz>
2017-03-17 18:35:22 +0100

    3.6 works, now fix for bragging and let's go home

--- plugins/runescape/blueprint.py
+++ plugins/runescape/blueprint.py
@@ -3,13 +3,14 @@
 from flask import Blueprint, json
 from .player import Player
 
-rs_bp = Blueprint('RuneScape', 'runescape', url_prefix='/rs/')
+rs_bp = Blueprint('RuneScape', 'runescape', url_prefix='/rs')
 
 @rs_bp.route('/')
 def index():
     return 'coming soon-ish'
 
 
+@rs_bp.route('/api/player/')
 @rs_bp.route('/api/player/<string:name>')
 def player(name='DJetelina'):
     return json.jsonify(Player(name).__dict__)
